eebddbc13c5e15396e0fa67cc53a0c33539617ce
Prarthana <prarthanabhatt497@gmail.com>
2021-03-02 22:11:58 +0530

    TextView DataBinding

---
+++ app/src/main/java/com/prarthana/datamapping/MainActivity.java
@@ -0,0 +1,19 @@
+package com.prarthana.datamapping;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import android.os.Bundle;
+
+import com.prarthana.datamapping.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+        binding.setUser(new User("Prarthana","Bhatt"));
+    }
+}
